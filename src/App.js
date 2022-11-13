36ae6dfcd22692a173d80cb92669bf9be11036dd
deniPamungkas <pamungkasdeni18@gmail.com>
2022-11-13 21:37:16 +0700

    tambah remote

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>FORM VALIDATION merge master branch2</h1>
+      <h1>FORM VALIDATION remote</h1>
     </div>
   );
 }
